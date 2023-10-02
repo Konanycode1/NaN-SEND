1627190ffdba4a9628ccffb0442507c23548e874
ANNEMARIE05 <annemarieagbonou@gmail.com>
2023-10-02 17:03:47 +0000

    fini fil

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Fil from './composants/fil'
+import Message from './composants/message'
 
 function App() {
 
 
   return (
-
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/fil" element={<Fil/>}/>
+        <Route path="/" element={<Message/>}/>
+      </Routes>
+    </BrowserRouter>
+    </>
   )
 }
 
--- src/composants/fil.jsx
+++ src/composants/fil.jsx
@@ -1,11 +1,34 @@
-import "../css/fil.css"
+import "../assets/css/fil.css"
+import images from "../assets/images/whatsapp1.png"
 
 
 
 export default function Fil() {
     return(
         <>
-        
+            <div className="cardFils">
+                <div class="row">
+                <div class="col-sm-8 mb-3 mb-sm-0">
+                    <div class="card filcardbody">
+                        <div class="card-body">
+                            <h5 class="card-title">BOÎTE DE RÉCEPTION DES MESSAGES</h5>
+                        </div>
+                        <div class="card-body col-sm-6 mx-auto mt-5 filcard">
+                            <h3>Un interlocuteur, <span>un fil</span></h3>
+                            <p>Un témoignage client unique contenant chaque message, e-mail, chat Web et discussion interne. </p>
+                            <p>Toutes les fonctionnalités que vous attendez et certaines que vous ne trouverez nulle part ailleurs.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 ">
+                    <div class="card cardFilImg">
+                    <div class="card-body ">
+                        <img src={images} alt="" />
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
         </>
     )
 }
