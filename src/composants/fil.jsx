af30d4041f1cd089d857138eb20229f583206282
ANNEMARIE05 <annemarieagbonou@gmail.com>
2023-10-03 17:01:41 +0000

    terminer ma partie

--- src/composants/fil.jsx
+++ src/composants/fil.jsx
@@ -2,27 +2,26 @@ import "../assets/css/fil.css"
 import images from "../assets/images/whatsapp1.png"
 
 
-
 export default function Fil() {
     return(
         <>
             <div className="cardFils">
-                <div class="row">
-                <div class="col-sm-8 mb-3 mb-sm-0">
-                    <div class="card filcardbody">
-                        <div class="card-body">
-                            <h5 class="card-title">BOÎTE DE RÉCEPTION DES MESSAGES</h5>
+                <div className="row">
+                <div className="col-sm-7 mb-3 mb-sm-0">
+                    <div className="card filcardbody">
+                        <div className="card-body">
+                            <h5 className="card-title">BOÎTE DE RÉCEPTION DES MESSAGES</h5>
                         </div>
-                        <div class="card-body col-sm-6 mx-auto mt-5 filcard">
+                        <div className="card-body col-sm-6 mx-auto mt-5 filcard">
                             <h3>Un interlocuteur, <span>un fil</span></h3>
                             <p>Un témoignage client unique contenant chaque message, e-mail, chat Web et discussion interne. </p>
                             <p>Toutes les fonctionnalités que vous attendez et certaines que vous ne trouverez nulle part ailleurs.</p>
                         </div>
                     </div>
                 </div>
-                <div class="col-sm-4 ">
-                    <div class="card cardFilImg">
-                    <div class="card-body ">
+                <div className="col-sm-4 ">
+                    <div className="card cardFilImg">
+                    <div className="card-body ">
                         <img src={images} alt="" />
                     </div>
                     </div>
--- src/composants/message.jsx
+++ src/composants/message.jsx
@@ -1,26 +1,58 @@
 import "../assets/css/messages.css"
+import Fil from "./fil"
 
 
 
 export default function Message() {
     return(
+
         <>
+                <Fil />
             <div className="cardMessage">
-                <div class="row">
-                <div class="col-sm-6 mb-3 mb-sm-0">
-                    <div class="card">
-                    <div class="card-body">
-                        
-                    </div>
+                <div className="row">
+                <div className="col-sm-6 mb-3 mb-sm-0">
+                    <div className="card">
+                        <div className="card-body col-sm-7 mx-auto messageTelephone">
+                            <div className="telephontEcran">
+                                <div className="telephoneEntete">
+                                    <div className="telephoneRond">
+                                        <div></div>
+                                        <div></div>
+                                        <div></div>
+                                    </div>
+                                </div>
+                                <div className="telephoneMilieu">
+                                    <h4>Nouvelle diffusion</h4>
+                                    <label htmlFor="">Nom de la diffusion</label>
+                                    <input type="text" placeholder="Promo Nan 6.23"/>
+                                    <label htmlFor="">Public ciblé</label>
+                                    <select name="" id="">
+                                        <option value="">sélectionner un public</option>
+                                        <option value="">Teams Privés <i class="fa-solid fa-message"></i><i class="fa-solid fa-envelope"></i><i class="fa-brands fa-whatsapp"></i></option>
+                                        <option value="">Javascript teams</option>
+                                        <option value="">Python teams</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="col-sm-6">
-                    <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Special title treatment</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
+                <div className="col-sm-6">
+                    <div className="card">
+                        <div className="card-body mt-5">
+
+                            <div className="card-body col-sm-6 mt-5 filcard">
+                                <h6>MODULE DE DIFFUSION</h6>
+                                <h3>Messages en masse,</h3>
+                                <h3><span>livré efficacement</span></h3>
+                                <p>
+                                    Diffusion de routine et urgente de newsletters, 
+                                    de mises à jour d'événements, de diffusions d'urgence, 
+                                    d'alertes de temps d'arrêt et bien plus encore.
+                                </p>
+                            </div>
+                            
+                        </div>
                     </div>
                 </div>
                 </div>
