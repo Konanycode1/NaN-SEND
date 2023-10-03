e5474611faca593b925ea1bf808ffd7b0685c1e2
NaN-Didital <nan-didital@192.168.50.67>
2023-10-03 17:17:33 +0000

    navbar

--- src/composants/NavBar.jsx
+++ src/composants/NavBar.jsx
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useRef } from "react";
 import "../assets/css/NavBar.css";
 import { Link } from "react-router-dom";
-import {BiSolidMessageRoundedDots} from "react-icons/bi";
+import { BiSolidMessageRoundedDots } from "react-icons/bi";
+import { TfiMenuAlt } from "react-icons/tfi";
+import { RiEyeCloseLine } from "react-icons/ri";
 
 function NavBar() {
+  const nav = useRef(null);
+  const toggle = (event) => {
+    const close = document.getElementById("close");
+
+    close.style.display = "block";
+    close.addEventListener("click", (evn) => {
+      nav.current.classList.remove("active");
+      event.target.style.display = "block";
+      evn.target.style.display = "none";
+    });
+
+    event.target.style.display = "none";
+    console.log(event.target);
+    nav.current.classList.toggle("active");
+    
+  };
   return (
     <>
       <div className="laNav">
         <nav>
           <a href="#">
-            <span><BiSolidMessageRoundedDots/></span>
-            <span>NAN-</span>
-            <span>SEND</span>
+            <span className="reactIcon-logo">
+              <BiSolidMessageRoundedDots />
+            </span>
+            <span className="span1">NAN-</span>
+            <span className="span2">SEND</span>
           </a>
-          <ul className="navbar">
+          <ul className="navbar" ref={nav}>
             <li>
               <a href="" className="Acceuil-active">
                 Accueil
@@ -23,7 +43,7 @@ function NavBar() {
               <a href="">À propos</a>
             </li>
             <li>
-              <a href="">Service</a>
+              <a href="">Services</a>
             </li>
             <li>
               <a href="">Contact</a>
@@ -34,6 +54,12 @@ function NavBar() {
               Se connecter
             </Link>
           </div>
+          <span className="menuBurger" onClick={toggle} id="burger">
+            <TfiMenuAlt />
+          </span>
+          <span className="close" id="close">
+            <RiEyeCloseLine />
+          </span>
         </nav>
       </div>
     </>
