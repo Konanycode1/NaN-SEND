9263ba733606daf023ac79a8fdb6a1dd0d5ca20f
NaN-Didital <nan-didital@192.168.50.67>
2023-10-06 17:19:56 +0000

    slid

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
-import React, { Suspense } from 'react';
-import './App.css';
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
-import NavBar from './composants/NavBar';
-import ComposantBienvenu from './composants/ComposantBienvenu';
-import Login from './assets/css/Login';
-import Register from './assets/css/Register';
-
+import React, { Suspense } from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+import NavBar from "./composants/NavBar";
+import ComposantBienvenu from "./composants/ComposantBienvenu";
+import Login from "./assets/css/Login";
+import Register from "./assets/css/Register";
+import Slider from "./composants/Slider";
 
 function App() {
-  return(
+  return (
     <>
-    <BrowserRouter>
-    <Routes>
-       <Route path='/'element={<NavBar/>} />
-       <Route path='ComposantBienvenu'element={<ComposantBienvenu/>} />
-       <Route path='Login'element={<Login/>} />
-       <Route path='Register'element={<Register/>} />
-    </Routes>
-    </BrowserRouter> 
+      <BrowserRouter>
+        <Routes>
+          <Route path="/NavBar" element={<NavBar />} />
+          <Route path="/ComposantBienvenu" element={<ComposantBienvenu />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/Slider" element={<Slider/>} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/composants/Slider.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../assets/css/Slider.css"
+import tph from 
+
+function Slider(){
+    return (
+        <div></div>
+    )
+}
+export default Slider;
