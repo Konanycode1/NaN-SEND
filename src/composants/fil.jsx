2fd1f255238e2f495ced0d1dce470fab4a879e0f
Diarassouba Mamadou <couda.dm@gmail.com>
2023-10-12 09:26:36 +0000

    ok

--- src/composants/fil.jsx
+++ src/composants/fil.jsx
@@ -1,33 +1,40 @@
-import "../assets/css/fil.css"
-import images from "../assets/images/whatsapp1.png"
-
+import "../assets/css/fil.css";
+import images from "../assets/images/whatsapp1.png";
 
 export default function Fil() {
-    return(
-        <>
-            <div className="cardFils">
-                <div className="row">
-                <div className="col-sm-7 mb-3 mb-sm-0">
-                    <div className="card filcardbody">
-                        <div className="card-body">
-                            <h5 className="card-title">BOÎTE DE RÉCEPTION DES MESSAGES</h5>
-                        </div>
-                        <div className="card-body col-sm-6 mx-auto mt-5 filcard">
-                            <h3>Un interlocuteur, <span>un fil</span></h3>
-                            <p>Un témoignage client unique contenant chaque message, e-mail, chat Web et discussion interne. </p>
-                            <p>Toutes les fonctionnalités que vous attendez et certaines que vous ne trouverez nulle part ailleurs.</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-sm-4 ">
-                    <div className="card cardFilImg">
-                    <div className="card-body ">
-                        <img src={images} alt="" />
-                    </div>
-                    </div>
-                </div>
-                </div>
+  return (
+    <>
+      <div className="cardFils">
+        <div className="row">
+          <div className="col-sm-7 mb-3 mb-sm-0">
+            <div className="card filcardbody">
+              <div className="card-body">
+                <h5 className="card-title">BOÎTE DE RÉCEPTION DES MESSAGES</h5>
+              </div>
+              <div className="card-body col-sm-6 mx-auto mt-5 filcard">
+                <h3>
+                  Un interlocuteur, <span>un fil</span>
+                </h3>
+                <p>
+                  Un témoignage client unique contenant chaque message, e-mail,
+                  chat Web et discussion interne.{" "}
+                </p>
+                <p>
+                  Toutes les fonctionnalités que vous attendez et certaines que
+                  vous ne trouverez nulle part ailleurs.
+                </p>
+              </div>
             </div>
-        </>
-    )
+          </div>
+          <div className="col-sm-4 ">
+            <div className="card cardFilImg">
+              <div className="card-body ">
+                <img src={images} alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
